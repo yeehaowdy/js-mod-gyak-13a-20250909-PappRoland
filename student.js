c34dd38183101426ef6457b67ce72723b2d341a2
Roland Papp <pappr2006@gmail.com>
2025-09-09 17:07:05 +0200

    complete and test task 4

--- student.js
+++ student.js
@@ -1,7 +1,18 @@
 //student
 import Person from "./person";
 
-class Student extends Person{{
+export default class Student extends Person{
     _marks = [];
     
+    constructor(marks){
+        super();
+        var addMarks = [5,4,5];
+        this._marks = [...addMarks, ...marks]
+
+
+    }
 }
+
+//test task 4
+let student = new Student([]);
+console.log(student);
