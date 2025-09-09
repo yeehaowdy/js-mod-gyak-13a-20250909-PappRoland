c5f2ef31c2d517b1ac95405fc24221b209c6b54e
Roland Papp <pappr2006@gmail.com>
2025-09-09 16:48:43 +0200

    complete task 2, write test for constructor

--- person.js
+++ person.js
@@ -2,9 +2,20 @@
 export default class Person{
     _name;
 
-    constructor(_name){
-        this._name = _name;
+    constructor(name){
+        this._name = name;
 
-        
+    }
+    get name(){
+        return this._name;
+
+    }
+
+    set name(name){
+        this._name = name;
+
     }
 }
+//test
+let person = new Person('john guy');
+console.log(person.name);
